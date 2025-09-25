ee1e0e85fd3603a7715c84d70286d66f25c5035b
Aditya24-rathore <ra4402538@gmail.com>
2025-09-25 03:59:16 -0700

    update

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,15 +1,38 @@
 import React, { useState } from 'react'
 
 const Form = () => {
-    const [name,setname]=useState()
-    let change=(e)=>{
-     setname(e.target.value)
-    }
+    const [name,setname]=useState(" ")
+    const [email,setemail]=useState(" ")
+    const [num,setnum]=useState(" ")
+    const [pass,setpass]=useState(" ")
     
+
+    // let change=(e)=>{
+    //  setname(e.target.value)
+    // }
+
+    
+    let handlesubmit=(e)=>{
+      // preventDefault to stop form reloading or refresh
+      e.preventDefault()
+      if(name==""){
+        alert("enter name")
+      }
+      else{alert("Registration Done")}  
+    }
   return (
     <>
-    <h1>{name}</h1>
-    <input type="text" onChange={change}/>
+    <form onSubmit={handlesubmit}>
+      <h1>{name}</h1>
+    Enter name:<input type="text" value={name} onChange={(e)=>{setname(e.target.value)}}/> <br /> <br />
+    <h1>{email}</h1>
+     Enter email:<input type="text" value={email} onChange={(e)=>{setemail(e.target.value)}}/> <br /> <br />
+     <h1>{num}</h1>
+    Enter number:<input type="text" value={num} onChange={(e)=>{setnum(e.target.value)}}/> <br /> <br />
+    <h1>{pass}</h1>
+     Enter password:<input type="text" value={pass} onChange={(e)=>{setpass(e.target.value)}}/> <br /> <br />
+    <button type='submit'>Submit</button>
+    </form>
     </>
   )
 }

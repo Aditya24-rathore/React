c1a948e9ae19b6acf2b8fac6a29062666b95abea
Aditya24-rathore <ra4402538@gmail.com>
2025-10-07 03:59:38 -0700

    update

--- src/Propdrilling.jsx
+++ src/Propdrilling.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import mycontext from './mycontext'
 import { createContext } from 'react'
 import { useContext } from 'react'
@@ -6,12 +6,26 @@ import { useContext } from 'react'
 
 
 const Propdrilling = () => {
-    const data=useContext(mycontext)
+    // const data=useContext(mycontext)
+    // const theme=useContext(mycontext)
+    // const {num1,num2}=useContext(mycontext)
+    // const {num}=useContext(mycontext)
+    const theme=useContext(mycontext)
+    const[mytheme,setmytheme]=useState(false)
+  
+  
   return (
     <>
-    <h1>My name is {data.name}</h1>
-    <p>My age is {data.age}</p>
-    <p>My city is {data.city}</p>
+    {/* <h1>My name is {mydata.name}</h1>
+    <p>My age is {mydata.age}</p>
+    <p>My city is {mydata.city}</p>
+    <p>My hobby is {hobby[0]}</p>
+    <p>sum of two num is {num.num1+num.num2}</p> */}
+
+    <div style={theme}>
+
+       <button onClick={()=>{setmytheme(mytheme?theme.dark:theme.light)}}>Click</button>
+    </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,32 @@ import { createRoot } from 'react-dom/client'
 import App from './Propdrilling.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import mycontext from './mycontext.jsx'
-
 let mydata={
     name:"aditya",
     age:"20",
     city:"Bhopal",
 }
 
+let hobby=["reading","writing","singing"]
+
+let theme={
+    dark:{
+        "background-color":"black",
+        "color":"white"
+    },
+    light:{
+        "background-color":"white",
+        "color":"black"
+    }
+}
+
+let num={
+    "num1":20,
+    "num2":30
+}
+
 createRoot(document.getElementById('root')).render(
-    <mycontext.Provider value={mydata}>
+    <mycontext.Provider value={{theme}}>
    <BrowserRouter>
     <App />
     </BrowserRouter>
